4ac4376d60ebcecb25c9a9e9e922dc1d3f2393ee
Serene <andyhsu031@gmail.com>
2020-04-19 21:24:06 -0400

    modified download

--- metube_template/media_download_process.php
+++ metube_template/media_download_process.php
@@ -14,7 +14,22 @@ $mediaid=$_REQUEST['id'];
 //insert into upload table
 $insertDownload="insert into download(downloadid,username,mediaid) values(NULL,'$username','$mediaid')";
 $queryresult = mysql_query($insertDownload)
-	
+
+$query="SELECT * FROM media WHERE mediaid=$ID";
+$result=mysql_query($query);
+$filepath = $result->filepath; 
+$filetype = $result->type;
+$filename = $result->filename;
+
+if(file_exists)){
+    header("Pragma: public");
+    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+    header("Content-Type: ".$filetype);
+    header("Content-Length: ".filesize($filepath));
+    readfile($filepath);
+}
+
 ?>
+
 
 
