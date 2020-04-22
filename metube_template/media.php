ba8387cd6aa798450c076b264369e969d634c89d
Serene <andyhsu031@gmail.com>
2020-04-21 21:32:35 -0400

    Test change

--- metube_template/media.php
+++ metube_template/media.php
@@ -106,7 +106,7 @@ if(isset($_GET['id'])) {
 ?>
 	<p>Viewing Video:<?php echo $result_row[2].$result_row[1];?></p>
 
-    <object id="MediaPlayer" width=320 height=286 classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95" standby="Loading Windows Media Player components…" type="application/x-oleobject" codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=6,4,7,1112">
+    <!-- <object id="MediaPlayer" width=320 height=286 classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95" standby="Loading Windows Media Player components…" type="application/x-oleobject" codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=6,4,7,1112">
 
 <param name="filename" value="<?php echo $result_row[2].$result_row[1];  ?>">
 <param name="Showcontrols" value="True">
@@ -114,12 +114,11 @@ if(isset($_GET['id'])) {
 
 <embed type="application/x-mplayer2" src="<?php echo $result_row[2].$result_row[1];  ?>" name="MediaPlayer" width=320 height=240></embed>
 
-</object>
+</object> -->
 
-
-
-
-
+	<video controls width="250">
+		<source src="<?php echo $result_row[2].$result_row[1];  ?>"
+			type="video/wmv">
 
 <?php
 	}
